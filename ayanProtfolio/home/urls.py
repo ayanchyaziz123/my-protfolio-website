898b7834d649abd1a90422da968c14f5591b4b61
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2021-03-08 23:39:17 +0600

    edit a tag

--- ayanProtfolio/home/urls.py
+++ ayanProtfolio/home/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
 
      path('', views.home, name='home'),
+     path('skill/', views.skill, name='skill'),
+     path('contact/', views.contact, name='contact'),
 ]
--- ayanProtfolio/home/views.py
+++ ayanProtfolio/home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def skill(request):
+    return render(request, 'skill.html')    
+def contact(request):
+    return render(request, 'contact.html')     
