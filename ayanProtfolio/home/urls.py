644e2f0c4039f2325665932f68d6f267f0ec1841
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2021-06-03 15:37:49 +0600

    completed

--- ayanProtfolio/home/urls.py
+++ ayanProtfolio/home/urls.py
@@ -5,14 +5,6 @@ from . import views
 urlpatterns = [
 
      path('', views.home, name='home'),
-     path('fullProject/<str:slug>', views.fullProject, name='fullProject'),
-     path('skill/', views.skill, name='skill'),
      path('contact/', views.contact, name='contact'),
-     path('certificate/', views.certificate, name='certificate'),
-     path('education/', views.education, name='education'),
-     path('testimonial/', views.testimonial, name='testimonial'),
-     path('research_paper/', views.research_paper, name='research_paper'),
-     path('exprience/', views.exprience, name='exprience'),
-     path('achievement/', views.achievement, name='achievement'),
 
 ]
--- ayanProtfolio/home/views.py
+++ ayanProtfolio/home/views.py
@@ -22,30 +22,6 @@ def home(request):
         'total_recom': total_recom,
     }
     return render(request, 'home.html', context)
-def fullProject(request, slug):
-    projects = Project.objects.filter(id=slug).first()  
-    project = Project.objects.all()
-    paginator = Paginator(project, 8)
-    page = request.GET.get('page')
-    project = paginator.get_page(page)
-    context = {
-        'projects':projects,
-        'project':project,
-    }
-    return render(request, 'fullProject.html', context)
-
-def skill(request):
-    project = Project.objects.all()
-    paginator = Paginator(project, 8)
-    page = request.GET.get('page')
-    project = paginator.get_page(page)
-    data = Skill.objects.all()
-    context = {
-        'project':project,
-        'data': data,
-    }
-    return render(request, 'body.html', context)    
-
 
 
 def contact(request):
@@ -68,76 +44,4 @@ def contact(request):
             'project':project,
         }
     return render(request, 'contact.html', context)  
-
-def education(request):
-    project = Project.objects.all()
-    paginator = Paginator(project, 8)
-    page = request.GET.get('page')
-    project = paginator.get_page(page)
-    data = Education.objects.all()
-    context = {
-        'project':project,
-        'data': data,
-    }
-    return render(request, 'body.html', context)          
-
-def certificate(request):
-    project = Project.objects.all()
-    paginator = Paginator(project, 8)
-    page = request.GET.get('page')
-    project = paginator.get_page(page)
-    data = Certificate.objects.all()
-    context = {
-        'project':project,
-        'data': data,
-    }
-    return render(request, 'body.html', context)  
-
-def testimonial(request):
-    project = Project.objects.all()
-    paginator = Paginator(project, 8)
-    page = request.GET.get('page')
-    project = paginator.get_page(page)
-    data = Testimonial.objects.all()
-    context = {
-        'project':project,
-        'data': data,
-    }
-    return render(request, 'body.html', context) 
-
-def research_paper(request):
-    project = Project.objects.all()
-    paginator = Paginator(project, 8)
-    page = request.GET.get('page')
-    project = paginator.get_page(page)
-    data = ResearchPaper.objects.all()
-    context = {
-        'project':project,
-        'data': data,
-    }
-    return render(request, 'body.html', context) 
-
-def exprience(request):
-    project = Project.objects.all()
-    paginator = Paginator(project, 8)
-    page = request.GET.get('page')
-    project = paginator.get_page(page)
-    data = Exprience.objects.all()
-    context = {
-        'project':project,
-        'data': data,
-    }
-    return render(request, 'body.html', context) 
-
-def achievement(request):
-    project = Project.objects.all()
-    paginator = Paginator(project, 8)
-    page = request.GET.get('page')
-    project = paginator.get_page(page)
-    data = Achievement.objects.all()
-    context = {
-        'project':project,
-        'data': data,
-    }
-    return render(request, 'body.html', context) 
 
